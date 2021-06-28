8ec2a7f02fbbc07c7e2aa1b86dcd4a3c1ac8d1e6
thevioletmaniac <thevioletmaniac@gmail.com>
2021-06-28 17:58:51 +0600

    Add constants: default currencies, default categories

---
+++ src/constants/defaultCategories.js
@@ -0,0 +1,16 @@
+export default {
+  expense: [
+    'food',
+    'transportation',
+    'personal',
+    'entertainment',
+    'gifts',
+    'education',
+    'healthcare',
+    'travel',
+    'housing',
+    'bills',
+    'clothing',
+  ],
+  income: ['salary', 'cashback', 'interest'],
+};
---
+++ src/constants/defaultCurrencies.js
@@ -0,0 +1,1 @@
+export default ['RUB', 'USD', 'EUR'];
