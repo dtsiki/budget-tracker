f18dd73d3693f4919af8ae39e9a94677c638c734
thevioletmaniac <thevioletmaniac@gmail.com>
2021-07-02 16:46:03 +0600

    Adjust Button component: fix style, add icon

--- src/components/base/Button/Button.js
+++ src/components/base/Button/Button.js
@@ -3,10 +3,27 @@ import React from 'react';
 
 import './style.scss';
 
-const Button = ({ children, onClick, type = 'submit', variant = 'primary' }) => {
+const Button = ({
+  children,
+  onClick,
+  type = 'submit',
+  variant = 'primary',
+  icon,
+  iconClassName,
+  isDisabled = false,
+  isHiddenLabel = false,
+  customClassName,
+}) => {
   return (
-    <button onClick={onClick} type={type} className={`button button--${variant}`}>
-      {children}
+    <button
+      onClick={onClick}
+      type={type}
+      className={`button button--${variant}${isDisabled ? ' button$--disabled' : ''}${
+        customClassName ? ` ${customClassName}` : ''
+      }`}
+    >
+      {icon && <span className={`button__icon ${iconClassName || ''}`}>{icon}</span>}
+      <span className={`button__label${isHiddenLabel ? ' button__label--hidden' : ''}`}>{children}</span>
     </button>
   );
 };
@@ -16,6 +33,11 @@ Button.propTypes = {
   onClick: PropTypes.func,
   type: PropTypes.string,
   variant: PropTypes.string,
+  icon: PropTypes.node,
+  iconClassName: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+  isHiddenLabel: PropTypes.bool,
+  customClassName: PropTypes.string,
 };
 
 export default Button;
